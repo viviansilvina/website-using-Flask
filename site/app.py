22f97f87d8f9844be3ae30252cff97d7454f85f7
viviansilvina <viviansilvina23@GMAIL.COM>
2024-08-11 03:34:28 -0300

    testes de rotas

--- site/app.py
+++ site/app.py
@@ -6,6 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
